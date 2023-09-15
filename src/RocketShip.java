7b7f72b6a80dbe5e2e80630944f1061c31a6e6cd
Arturo Valle <arturog.valle@calbaptist.edu>
2023-09-14 22:20:40 -0700

    initial version

---
+++ src/RocketShip.java
@@ -0,0 +1,27 @@
+public class RocketShip {
+    public static final int SIZE = 8;
+
+    public RocketShip() { /* compiled code */ }
+
+    public static void main(java.lang.String[] args) {
+        top();
+        borderOne();
+        bodyOne();
+        bodyTwo();
+        borderTwo();
+        bodyThree();
+        bodyFour();
+        borderThree();
+        bottom();
+    }
+
+    public static void top() { /* compiled code */}
+    public static void borderOne() { /* compiled code */ }
+    public static void bodyOne() { /* compiled code */ }
+    public static void bodyTwo() { /* compiled code */ }
+    public static void borderTwo() { /* compiled code */ }
+    public static void bodyThree() { /* compiled code */ }
+    public static void bodyFour() { /* compiled code */ }
+    public static void borderThree() { /* compiled code */ }
+    public static void bottom() { /* compiled code */ }
+}
